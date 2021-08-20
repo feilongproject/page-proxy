0732e026585f2715b7eb100613ffbba950849903
飞龙project <flzs6137@gmail.com>
2021-08-20 10:57:22 +0800

    修复favicon.ico失效问题

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ async function handleRequest(request) {
 	const text = (await html.text())
 		.replaceAll("files.yande.re", file_url)
 		.replaceAll("assets.yande.re", assets_url)
+		.replaceAll("/favicon.ico", "https://cdn.jsdelivr.net/gh/feilongproject/yande-proxy/favicon.ico")
+
     
 	return new Response(text, {
         headers: {
