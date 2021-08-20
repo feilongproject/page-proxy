8bcd6ae4861dd3629c6304e2270ce7c4c0b313ee
飞龙project <flzs6137@gmail.com>
2021-08-20 12:26:31 +0800

    更改headers里的content-type为源中指定

--- index.js
+++ index.js
@@ -13,20 +13,22 @@ async function handleRequest(request) {
 	const path = requestURL.pathname
 
     //const ttt = path + " " + requestURL + " 2 " + requestURL.search;
-    //return new Response(ttt)
 
 	const link = "https://" + "yande.re" + path + requestURL.search
 	const html = await fetch(link)
+
+	const contentType = html.headers.get("content-type")
+
+	//return new Response(contentType)
 
 	const text = (await html.text())
 		.replaceAll("files.yande.re", file_url)
 		.replaceAll("assets.yande.re", assets_url)
 		.replaceAll("/favicon.ico", "https://cdn.jsdelivr.net/gh/feilongproject/yande-proxy/favicon.ico")
-
     
 	return new Response(text, {
         headers: {
-            "content-type": "text/html;charset=UTF-8",
+            "content-type": contentType,
         },
     })
 }
